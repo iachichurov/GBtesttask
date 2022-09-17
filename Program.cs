8afd179538a955ffe625d4bd07b6e2afce332990
iachichurov <a.iachichurov@gmail.com>
2022-09-18 04:36:45 +1000

    Task complete

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] array = {"hello", "2", "world", ":-E"};
+int counter = 0;
+string[] result = new string[0];
+
+foreach(string str in array)
+{
+    if(str.Length <= 3)
+    {
+        Array.Resize(ref result, result.Length+1);
+        result[counter] = str;
+        counter++;
+    }
+}
+Console.WriteLine("[{0}]", string.Join(", ", result));
